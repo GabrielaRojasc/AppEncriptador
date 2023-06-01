5e2e6cf827c37ac3c2e732b6e52060d4f6d03d28
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-06-01 11:22:53 -0600

    Add new fuction encriptar texto, con un ciclo for

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ function recuperarTexto(){
     return cajatexto.value
 }
 
+function encriptar(){
+    ocultarAdelante();
+    let cajatexto = recuperarTexto()
+    resultado.textContent = encriptarTexto(cajatexto);
+}
+
+function encriptarTexto(mensaje){
+    let texto = mensaje;
+    let textoFinal = "";
+
+    for(let i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
