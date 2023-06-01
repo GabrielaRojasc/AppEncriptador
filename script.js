27e85ff157a0618671cf8f90b1ff1c45062d1a60
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-06-01 11:24:40 -0600

    Add new fuction dencriptar texto, con un ciclo for + funcion desencriptar

--- script.js
+++ script.js
@@ -51,3 +51,39 @@ function encriptarTexto(mensaje){
 
 }
 
+function desencriptarTexto(mensaje){
+    let texto = mensaje;
+    let textoFinal = "";
+
+    for(let i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+        
+    }
+
+    return textoFinal;
+
+}
+
