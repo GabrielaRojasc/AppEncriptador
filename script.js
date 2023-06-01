30ebda85975ccc5b524c80e37587789d786127e7
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-06-01 11:18:55 -0600

    Add variables necesarias para iniciar, agregando querySelector para llamarlo del html

---
+++ script.js
@@ -0,0 +1,6 @@
+let botonEncriptar = document.querySelector(".btn-encriptar");
+let botonDesencriptar = document.querySelector(".btn-desencriptar");
+let munieco = document.querySelector(".contenedor-munieco");
+let contenedor = document.querySelector(".contenedor-parrafo");
+let resultado = document.querySelector(".texto-resultado");
+
