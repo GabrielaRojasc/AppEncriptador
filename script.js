b81b33fa23a1bd663631d54cebe88fea740e1639
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-06-01 11:21:15 -0600

    Add new fuction para ocultar la imagen, function traer el texto, y el darle click a los botones

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let munieco = document.querySelector(".contenedor-munieco");
 let contenedor = document.querySelector(".contenedor-parrafo");
 let resultado = document.querySelector(".texto-resultado");
 
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+
+function ocultarAdelante(){
+    munieco.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function recuperarTexto(){
+    let cajatexto = document.querySelector(".caja-texto")
+    return cajatexto.value
+}
+
